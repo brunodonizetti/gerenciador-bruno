3fea01941dd340dbc59077994e1feee9ab4c18be
brunodonizetti <bruno@fatecguaratingueta.edu.br>
2014-03-07 01:36:37 -0300

    dir_alura_curso

---
+++ dao/UsuarioDAO.java
@@ -0,0 +1,27 @@
+package br.com.alura.gerenciador.dao;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.alura.gerenciador.Usuario;
+
+public class UsuarioDAO {
+
+	private final static Map<String, Usuario> USUARIOS = new HashMap<>();
+	static {
+		USUARIOS.put("bruno@gmail.com", new Usuario("bruno@gmail.com","silva"));
+		USUARIOS.put("allbert@gmail.com", new Usuario("allbert@gmail.com","Almeida"));
+	}
+
+	public Usuario buscaPorEmailESenha(String email, String senha) {
+		if (!USUARIOS.containsKey(email))
+			return null;
+
+		Usuario usuario = USUARIOS.get(email);
+		if (usuario.getSenha().equals(senha))
+			return usuario;
+		
+		return null;
+	}
+
+}
